d5748284f7e845e374b36cd8d7102eb45b715159
Samuel Ridler <samridler@hotmail.com>
2024-02-17 18:44:06 +1300

    Add history button functionality

--- src/App.js
+++ src/App.js
@@ -55,15 +55,17 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    setHistory([...history.slice(0, currentMove + 1), nextSquares]);
+    jumpTo(currentMove + 1);
   }
 
   function jumpTo(nextMove) {
-    // TODO
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
   }
 
   const moves = history.map((squares, move) => {
