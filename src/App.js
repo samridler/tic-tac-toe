586cd38ae7be91ce9782797626a9c6a12285f142
Samuel Ridler <samridler@hotmail.com>
2024-02-17 18:46:09 +1300

    Remove unneeded state

--- src/App.js
+++ src/App.js
@@ -53,19 +53,18 @@ function Board({ xIsNext, squares, onPlay }) {
 }
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
     setHistory([...history.slice(0, currentMove + 1), nextSquares]);
-    jumpTo(currentMove + 1);
+    setCurrentMove(currentMove + 1);
   }
 
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
-    setXIsNext(nextMove % 2 === 0);
   }
 
   const moves = history.map((squares, move) => {
